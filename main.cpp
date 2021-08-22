36c63ba3c5d78c68bbcc7acd340fc81cc00dd8ac
cocodery <chriswuyyy123@gmail.com>
2021-08-23 02:24:36 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
-#include <iostream>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main() {
-	std::cout << "hello world" << std::endl;
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+	int opt = 0;
+	bool print_usage = false;
+	char *src = nullptr, *output = nullptr, *irfile = nullptr;
+
+  	for (int ch; (ch = getopt(argc, argv, "Sl:o:O:h")) != -1;) {
+		printf("%c\n",ch);
+		switch (ch) {
+			case 'S':
+				//do nothing
+				break;
+			case 'l':
+				irfile = strdup(optarg);
+				break;
+			case 'o':
+				output = strdup(optarg);
+				break;
+			case 'O':
+				opt = atoi(optarg);
+				opt = opt > 0 ? opt : 0;
+				break;
+			case 'h':
+				print_usage = true;
+				break;
+			default:
+				break;
+		}
+	}
+
+	if (optind <= argc) {
+		src = argv[optind];
+	}
+
 	return 0;
 }
 
