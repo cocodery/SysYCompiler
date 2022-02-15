49b1645493fadcd1089d4da45956b3594668fd32
cocodery <chriswuyyy123@gmail.com>
2022-02-16 02:10:19 +0800

    Mod File SymTable: add class InitValue, Variable for SymTable

--- src/structure/SymTable.cpp
+++ src/structure/SymTable.cpp
@@ -9,5 +9,11 @@ int32_t VarType::elements_number() {
     for (auto i : array_dims) {
         number *= i;
     }
-    return number;    
+    return number;
 }
+
+InitValue::InitValue() : is_const(false), is_array(false), scalar_init_value(0) {
+    list_init_value = vector<int32_t>(0);
+}
+
+Variable::Variable(VarType vt, InitValue iv) : type(vt), init(iv) { }
--- src/structure/SymTable.h
+++ src/structure/SymTable.h
@@ -17,3 +17,21 @@ public:
     VarType();
     int32_t elements_number();
 };
+
+class InitValue {
+public:
+    bool is_const;
+    bool is_array;
+    int32_t scalar_init_value;
+    vector<int32_t> list_init_value;
+
+    InitValue();
+};
+
+class Variable {
+public:
+    VarType type;
+    InitValue init;
+
+    Variable(VarType vt, InitValue iv);
+};
