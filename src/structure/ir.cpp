595492618164e1be92547a8326c399c6e52c3149
cocodery <chriswuyyy123@gmail.com>
2022-04-14 15:39:38 +0800

    Add: Init Lib_Function

--- src/structure/ir.cpp
+++ src/structure/ir.cpp
@@ -12,4 +12,26 @@ CompUnit::CompUnit() {
         lib_functions[i].libfunc_info.func_name = func_name[i];
         lib_functions[i].libfunc_info.return_type = ret_type[i];
     }
+    // getint
+    lib_functions[0].libfunc_info.func_args = vector<VarType>(0);
+    // getch
+    lib_functions[1].libfunc_info.func_args = vector<VarType>(0);
+    // getfloat
+    lib_functions[2].libfunc_info.func_args = vector<VarType>(0);
+    // getarray
+    lib_functions[3].libfunc_info.func_args.push_back(VarType(false, true, true, TypeInt));
+    // getfarray
+    lib_functions[4].libfunc_info.func_args.push_back(VarType(false, true, true, TypeFloat));
+    // putint
+    lib_functions[5].libfunc_info.func_args.push_back(VarType(false, false, true, TypeInt));
+    // putch
+    lib_functions[6].libfunc_info.func_args.push_back(VarType(false, false, true, TypeInt));
+    // putfloat
+    lib_functions[7].libfunc_info.func_args.push_back(VarType(false, false, true, TypeFloat));
+    // putarray
+    lib_functions[8].libfunc_info.func_args.push_back(VarType(false, false, true, TypeInt));
+    lib_functions[8].libfunc_info.func_args.push_back(VarType(false, true, true, TypeInt));
+    // putfarray
+    lib_functions[9].libfunc_info.func_args.push_back(VarType(false, false, true, TypeInt));
+    lib_functions[9].libfunc_info.func_args.push_back(VarType(false, true, true, TypeFloat));
 }
