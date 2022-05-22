024aca15243d592a39fb84676c10ffc70215166f
cocodery <chriswuyyy123@gmail.com>
2022-05-22 23:08:57 +0800

    Mod func parseNum, use sscanf

--- src/common.cpp
+++ src/common.cpp
@@ -1,19 +1,17 @@
 #include "common.hh"
 
 int parseNum(const char *number_str) {
-    int64_t ret = 0;
+    int32_t ret = 0;
     if (number_str[0] == '0' ) {
-        if (number_str[1] == 'x' || number_str[1] == 'X')
-            ret = strtoul(number_str, nullptr, 16);
-        else ret = strtoul(number_str, nullptr, 8);
-    }
-    else ret = strtoul(number_str, nullptr, 10);
-
-    if (ret > 2147483648ll) {
-        dbg("interger is too big to fit in int32_t");
-        exit(EXIT_FAILURE);
-    }
-    return static_cast<int32_t>(ret);
+        if (number_str[1] == 'x' || number_str[1] == 'X') {
+            sscanf(number_str, "%x", &ret);
+        } else {
+            sscanf(number_str, "%o", &ret);
+        }
+    } else {
+        sscanf(number_str, "%d", &ret);
+        }
+    return ret;
 }
 
 DeclType getDeclType(string type_str) {
