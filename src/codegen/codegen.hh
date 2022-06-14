ab9c809d4ff5d6c16b6fc2d656090acd8953b6dc
w43322 <raywang777@foxmail.com>
2022-06-14 22:15:35 +0800

    Update codegen.hh

--- src/codegen/codegen.hh
+++ src/codegen/codegen.hh
@@ -32,6 +32,8 @@ public:
         for (auto&& operand : operands)
             toReturn += " " + operand + ",";
         toReturn.pop_back();
+        if (!comment.empty())
+            toReturn += " ; " + comment; 
         return toReturn;
     }
     void PrintInstruction()
@@ -70,20 +72,19 @@ class CodeGenerator
 {
 public:
     Segment dataSegment;
+    Segment extraSegment;
     Segment codeSegment;
 public:
-    CodeGenerator() : dataSegment("DSEG"), codeSegment("CSEG") {}
+    CodeGenerator() : dataSegment("DSEG"), extraSegment("ESEG"), codeSegment("CSEG") {}
     void OutputToFile(string filePath)
     {
         // TODO
     }
-    void Generate(CompUnit &ir)
+    void AddInfoFromScope(const Scope& scope, bool iterateElems = true)
     {
-        cout << " - Generating DSEG:\n";
-        
-        cout << "   - Global Variable" << endl;
-        auto&& globalScope = *ir.global_scope;
-        auto&& varTable = globalScope.local_table->var_table;
+        auto&& varTable = scope.local_table->var_table;
+        auto&& elementPtrs = *scope.elements;
+
         for (auto&& var : varTable)
         {
             auto&& varName = var.first;
@@ -92,40 +93,175 @@ public:
             if (!varType.is_array && varType.decl_type == TypeInt)
             {
                 Instruction toInsert("DW",
-                                    {std::to_string(varContent.int_scalar)},
-                                    (string)"GLOBAL_" + (varContent.type.is_const?"CONST_":"VAR_")+varName);
+                                    {std::to_string(varContent.int_scalar % 65536)});
                 dataSegment.Insert(toInsert);
                 toInsert.PrintInstruction();
             }
         }
 
-        cout << "   - User Functions" << endl;
-        for (int i = 0, size = ir.functions.size(); i < size; ++i)
-        {
-            cout << "     - Funtion " << i << '\n';
-            auto&& func = *ir.functions[i];
-            auto&& funcInfo = func.func_info;
-            auto&& funcName = funcInfo.func_name;
-            
-            ir.functions[i]->printFunction();
-        }
+        if (!iterateElems)
+            return;
 
-        cout << " - Generating CSEG:\n";
-        cout << "  - Boilerplate\n";
+        for (auto&& elementPtr : elementPtrs)
+        {
+            if (Scope *scope_node = dynamic_cast<Scope *>(elementPtr); scope_node != nullptr)
+            {
+                AddInfoFromScope(*scope_node);
+            }
+            else
+            {
+                BasicBlock *bb_node = dynamic_cast<BasicBlock *>(elementPtr);
+                AddInfoFromBasicBlock(*bb_node);
+            }
+        }
+    }
+    void AddInfoFromBasicBlock(const BasicBlock& basicBlock)
+    {
+        for (auto inst: basicBlock.basic_block)
+        {
+            vector<Instruction> insts;
+            Case (ReturnInst, ret_inst, inst)
+            {
+                ret_inst->printRetInst();
+            }
+            Case (LoadNumber, ldc_inst, inst)
+            {
+                if (ldc_inst->src.type != TypeInt)
+                    continue;
+                insts.push_back(Instruction(
+                    "MOV", {
+                        "ES:[" + std::to_string(ldc_inst->dst.reg_id << 1) + "]",
+                        "WORD PTR " + std::to_string(ldc_inst->src.int_value % 65536)},
+                    "",
+                    false,
+                    "ld.c reg" + std::to_string(ldc_inst->dst.reg_id) + ", " + std::to_string(ldc_inst->src.int_value % 65536)));
+            }
+            Case (UnaryOpInst, uop_inst, inst)
+            {
+                insts.push_back(Instruction(
+                    "MOV", {
+                        "AX",
+                        "ES:[" + std::to_string(uop_inst->src.reg_id << 1) + "]"},
+                    "",
+                    false,
+                    "uop" + uop_inst->op.get_op() + " reg" + std::to_string(uop_inst->dst.reg_id) + ", reg" + std::to_string(uop_inst->src.reg_id)));
+                switch(uop_inst->op.unary_op)
+                {
+                case UnaryOp::Type::NOT:
+                    insts.push_back(Instruction("NOT", {"AX"}));
+                    break;
+                case UnaryOp::Type::NEG:
+                    insts.push_back(Instruction("NEG", {"AX"}));
+                    break;
+                case UnaryOp::Type::POS:
+                    insts.push_back(Instruction("POS", {"AX"}));
+                    break;
+                }
+                insts.push_back(Instruction("MOV", {"ES:[" + std::to_string(uop_inst->dst.reg_id << 1) + "]", "AX"}));
+            }
+            Case (BinaryOpInst, bop_inst, inst)
+            {
+                insts.push_back(Instruction(
+                    "MOV", {
+                        "AX",
+                        "ES:[" + std::to_string(bop_inst->src1.reg_id << 1) + "]"},
+                    "",
+                    false,
+                    "bop" + bop_inst->op.get_op() + " reg" + std::to_string(bop_inst->dst.reg_id) + ", reg" + std::to_string(bop_inst->src1.reg_id) + ", reg" + std::to_string(bop_inst->src2.reg_id)));
+                switch(bop_inst->op.bin_op)
+                {
+                case BinaryOp::Type::ADD:
+                    insts.push_back(Instruction("ADD", {"AX", "ES:[" + std::to_string(bop_inst->src2.reg_id << 1) + "]"}));
+                    break;
+                case BinaryOp::Type::SUB:
+                    insts.push_back(Instruction("SUB", {"AX", "ES:[" + std::to_string(bop_inst->src2.reg_id << 1) + "]"}));
+                    break;
+                case BinaryOp::Type::MUL:
+                    break;
+                case BinaryOp::Type::DIV:
+                    break;
+                case BinaryOp::Type::MOD:
+                    break;
+                case BinaryOp::Type::LTH:
+                    break;
+                case BinaryOp::Type::LEQ:
+                    break;
+                case BinaryOp::Type::EQU:
+                    break;
+                case BinaryOp::Type::NEQ:
+                    break;
+                case BinaryOp::Type::AND:
+                    break;
+                case BinaryOp::Type::ORR:
+                    break;
+                }
+                insts.push_back(Instruction("MOV", {"ES:[" + std::to_string(bop_inst->dst.reg_id << 1) + "]", "AX"}));
+            }
+            Case (AssignInst, ass_inst, inst)
+            {
+                insts.push_back(Instruction(
+                    "MOV", {
+                        "AX", 
+                        "ES:[" + std::to_string(ass_inst->src.reg_id << 1) + "]"},
+                    "",
+                    false,
+                    "ld.r reg" + std::to_string(ass_inst->dst.reg_id) + ", reg" + std::to_string(ass_inst->src.reg_id)));
+                insts.push_back(Instruction("MOV", {"BX", "ES:[" + std::to_string(ass_inst->dst.reg_id << 1) + "]"}));
+                insts.push_back(Instruction("MOV", {"DS:[BX]", "AX"}));
+            }
+            Case (LoadAddress, lad_inst, inst)
+            {
+                insts.push_back(Instruction(
+                    "MOV", {
+                        "ES:[" + std::to_string(lad_inst->dst.reg_id << 1) + "]",
+                        "WORD PTR " + std::to_string(lad_inst->variable->var_idx << 1)},
+                    "",
+                    false,
+                    "ld.v reg" + std::to_string(lad_inst->dst.reg_id) + ", *" + std::to_string(lad_inst->variable->var_idx)));
+            }
+            for (auto&& toInsert : insts)
+            {
+                codeSegment.Insert(toInsert);
+                toInsert.PrintInstruction();
+            }
+        }
+    }
+    void Generate(const CompUnit &ir)
+    {
+        cout << " - Boilerplate\n";
         vector<Instruction> boilerplate{
-            Instruction("ASSUME", {"CS:CSEG", "DS:DSEG"}),
+            Instruction("ASSUME", {"CS:CSEG", "DS:DSEG", "ES:ESEG"}),
             Instruction("MOV", {"AX", "DSEG"}, "START:", true),
-            Instruction("MOV", {"DS", "AX"})
+            Instruction("MOV", {"DS", "AX"}),
+            Instruction("MOV", {"AX", "ESEG"}),
+            Instruction("MOV", {"ES", "AX"})
         };
         for (auto&& toInsert : boilerplate)
         {
             codeSegment.Insert(toInsert);
             toInsert.PrintInstruction();
         }
-        
+
+
+        cout << " - Global Variable" << endl;
+        auto&& globalScope = *ir.global_scope;
+        AddInfoFromScope(globalScope, false);
+
+        cout << " - User Functions" << endl;
+        for (int i = 0, size = ir.functions.size(); i < size; ++i)
+        {
+            cout << "   - Funtion " << i << '\n';
+            auto&& func = *ir.functions[i];
+            auto&& funcInfo = func.func_info;
+            auto&& funcName = funcInfo.func_name;
+
+            auto&& mainScope = *func.main_scope;
+            AddInfoFromScope(mainScope);
+        }
 
         cout << " - Generated Code:\n";
         dataSegment.PrintSegment();
+        extraSegment.PrintSegment();
         codeSegment.PrintSegment();
         cout << "    END START\n";
     }
