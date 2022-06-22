39fdb2dba89e5a9b13f9c666d8628ac670f496aa
cocodery <chriswuyyy123@gmail.com>
2022-06-22 22:06:38 +0800

    Fix CTValue repeated constructor

--- src/structure/value.hh
+++ src/structure/value.hh
@@ -17,7 +17,6 @@ public:
     int32_t int_value;
     float float_value;
 public:
-    CTValue(): type(TypeVoid), int_value(0), float_value(0) { }
     CTValue(DeclType t = TypeVoid, int32_t iv = 0, float fv = 0);
 };
 
