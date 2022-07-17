a90072e55d4a209939aa71f6b99ab73db4d5985e
cocodery <chriswuyyy123@gmail.com>
2022-07-17 22:51:22 +0800

    Add optimizer class

---
+++ src/optimizer/pass_manager.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../common.hh"
+#include "../structure/ir.hh"
+
+class PassManager {
+public:
+    Scope *global_scope;
+    vector<Function *> functions;
+public:
+    PassManager(Scope *glb_scope, vector<Function *> funcs) : global_scope(glb_scope), functions(funcs) { }
+    void excute_pass() {
+        buildCFGs();
+    }
+    void buildCFGs();
+};
