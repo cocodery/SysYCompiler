329255773f9c8c105cd08893f6a05d2e7d7dea2d
cocodery <chriswuyyy123@gmail.com>
2022-08-01 04:06:25 +0800

    Add init lvnMapNodes without check

--- src/optimizer/lvn.cpp
+++ src/optimizer/lvn.cpp
@@ -1,22 +1,58 @@
 #include "lvn.hh"
 
 void LVN::runLVN() {
-    
+    auto &&all_blocks = function->all_blocks;
+    int32_t idx = 0;
+    for (auto &&block : all_blocks) {
+        bbIdxMapIdx.insert({block->bb_idx, idx++});
+        map<VirtReg *, SRC> reg2Src;
+        map<SRC, lvnNode *> reg2LvnNode;
+        replaceMap.push_back(reg2Src);
+        lvnMapNodes.push_back(reg2LvnNode);
+    }
+
+    for (auto &&block : all_blocks) {
+        block->dirty = false;
+        valueNumber(block);
+    }
 }
 
-void LVN::ValueNumber(vector<Inst> *bb) {
+void LVN::valueNumber(BasicBlock *block) {
+    int32_t mapIndex = bbIdxMapIdx[block->bb_idx];
+    for (auto &&iter = block->basic_block.begin(); iter != block->basic_block.end(); ++iter) {
+        auto &&inst = *iter;
+        Case (LLIR_BIN, bin_inst, inst) {
+            NodeOp op = NodeOp(bin_inst->op);
+            lvnNode *node = new lvnNode(op, bin_inst->src1, bin_inst->src2);
+            lvnMapNodes[mapIndex].insert({bin_inst->dst, node});
+        } else Case (LLIR_FBIN, fbin_inst, inst) {
+            NodeOp op = NodeOp(fbin_inst->op + 5);
+            lvnNode *node = new lvnNode(op, fbin_inst->src1, fbin_inst->src2);
+            lvnMapNodes[mapIndex].insert({fbin_inst->dst, node});
+        } else Case (LLIR_ICMP, icmp_inst, inst) {
+            NodeOp op = iCmp;
+            lvnNode *node = new lvnNode(op, icmp_inst->src1, icmp_inst->src2);
+            lvnMapNodes[mapIndex].insert({icmp_inst->dst, node});
+        } else Case (LLIR_FCMP, fcmp_inst, inst) {
+            NodeOp op = fCmp;
+            lvnNode *node = new lvnNode(op, fcmp_inst->src1, fcmp_inst->src2);
+            lvnMapNodes[mapIndex].insert({fcmp_inst->dst, node});
+        } else Case (LLIR_GEP, gep_inst, inst) {
+            NodeOp op = Gep;
+            lvnNode *node = new lvnNode(op, gep_inst->src, gep_inst->off);
+            lvnMapNodes[mapIndex].insert({gep_inst->dst, node});
+        } 
+    }
+}
+
+void LVN::ProcessInst(vector<Inst> *bb, Inst *ist, BinOp op, SRC dst, SRC src1, SRC src2) {
+}
+
+void LVN::FindTuple(tuple<int, int, string> t) {
 
 }
 
-int LVN::ProcessInst(vector<Inst> *bb, Inst *ist, BinOp op, SRC dst, SRC src1, SRC src2) {
-    return 0;
-}
-
-pair<int, string> LVN::FindTuple(tuple<int, int, string> t) {
-
-}
-
-int LVN::GetIndex(SRC dst) {
+void LVN::GetIndex(SRC dst) {
     
 }
 
--- src/optimizer/lvn.hh
+++ src/optimizer/lvn.hh
@@ -18,13 +18,19 @@ using std::string;
 using std::map;
 using std::tuple;
 
+enum NodeOp {  
+    Add, Sub, Mul, Div, Rem, 
+    fAdd, fSub, fMul, fDiv, 
+    iCmp, fCmp , Gep
+};
+
 class lvnNode {
 public:
-    BinOp op;
+    NodeOp op;
     SRC src1;
     SRC src2;
 public:
-    lvnNode(BinOp _op, SRC _src1, SRC _src2) : op(_op), src1(_src1), src2(_src2) { }
+    lvnNode(NodeOp _op, SRC _src1, SRC _src2) : op(_op), src1(_src1), src2(_src2) { }
 
     bool operator == (lvnNode node) {
         return op == node.op && src1 == node.src1 && src2 == node.src2;
@@ -34,18 +40,20 @@ public:
 class LVN{
 public:
     Function *function;
+    map<int32_t, int32_t> bbIdxMapIdx;
     vector<map<VirtReg *, SRC>> replaceMap;
-    vector<map<VirtReg *, lvnNode>> lvnNodes;
+    vector<map<SRC, lvnNode *>> lvnMapNodes;
 public:
     LVN(Function *func) : function(func) {
+        bbIdxMapIdx.clear();
         replaceMap.clear();
-        lvnNodes.clear();
+        lvnMapNodes.clear();
     }
     void runLVN();
-    void ValueNumber(vector<Inst> *bb);
-    int ProcessInst(vector<Inst> *bb, Inst *inst, BinOp op, SRC dst, SRC src1, SRC src2);
+    void valueNumber(BasicBlock *block);
+    void ProcessInst(vector<Inst> *bb, Inst *inst, BinOp op, SRC dst, SRC src1, SRC src2);
     void Remove(string dst, vector<Inst> *bb);
-    pair<int, string> FindTuple(tuple<int, int, string> t);
-    int GetIndex(SRC dst);
+    void FindTuple(tuple<int, int, string> t);
+    void GetIndex(SRC dst);
     void display();
 };
