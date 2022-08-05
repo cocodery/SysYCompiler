8826aa07b84d8400cbec35fa5be3c751a238a63d
Ray Wang <raywang777@foxmail.com>
2022-08-05 16:51:38 +0800

    fix function_recursion
    clear vis

--- src/optimizer/function_recursion.hh
+++ src/optimizer/function_recursion.hh
@@ -6,13 +6,13 @@
 class FunctionRecursion
 {
 public:
-    map<FunctionInfo *, Function *> info_to_function;
     set<FunctionInfo *> vis;
 public:
     FunctionRecursion(const vector<Function *> &functions) 
     {
         for (auto &&function : functions) {
             if (function->func_info.is_used) {
+                vis.clear();
                 function->func_info.is_recursive = IsRecursive(&function->func_info, &function->func_info);
             }
         }
