1cc12806e2c1623b4044eb8e6874e72d85453f84
w43322 <raywang777@foxmail.com>
2022-08-08 13:38:36 +0800

    make LLIR_LOAD compatible with s-regs

--- src/structure/baseclass.hh
+++ src/structure/baseclass.hh
@@ -14,6 +14,12 @@ class Inst: public Info {
 public:
     CLAIM_AVAIL_REGS
 public:
+    REGs GetFirstUnusedRRegister() const {
+        for (auto &&r : availRegs) {
+            if (r < s0) return r;
+            else return SPILL;
+        }
+    }
     virtual ~Inst() { }
 };
 
