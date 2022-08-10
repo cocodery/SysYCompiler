187780c9a8b2d297d99816e4f21b69a3c66a242e
Ray Wang <raywang777@foxmail.com>
2022-08-11 02:08:27 +0800

    fix store reordering

--- src/optimizer/load_store_reordering.hh
+++ src/optimizer/load_store_reordering.hh
@@ -6,7 +6,7 @@
 class LoadStoreReordering
 {
 public:
-
+    map<Inst *, int> void_call_map;
 public:
     LoadStoreReordering(Function *function);
     int ProcessInst(Inst* instPtr); // returns dst_regid
