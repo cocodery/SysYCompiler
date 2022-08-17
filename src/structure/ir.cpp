20e84d2a8972027995f24132e2423f709481ae32
cocodery <chriswuyyy123@gmail.com>
2022-08-17 15:53:25 +0800

    Del findInArgs-function, no more used

--- src/structure/ir.cpp
+++ src/structure/ir.cpp
@@ -194,15 +194,16 @@ SRC Scope::resolve(string var_name, FunctionInfo *cur_func_args) {
             if (var->type.is_array == false && var->type.is_const) {
                 return SRC(new CTValue(var->type.decl_type, var->int_scalar, var->float_scalar));
             }
+            // dbg(var_name, var->type.is_array);
             return SRC(new VirtReg(var->var_idx, var->type, (cur_scope->parent == nullptr)));
         }
-        if (cur_func_args != nullptr && cur_scope->parent->parent == nullptr) { // if not in table, search in function args
-            // cout << "not find in `Scope`" << cur_scope->sp_idx << " var_table, goto function arguments" << endl;
-            auto pair = cur_func_args->findInFuncArgs(var_name);
-            if (pair.second.decl_type != TypeVoid) {
-                return SRC(new VirtReg(pair.first, pair.second, (cur_scope->parent == nullptr)));
-            }
-        }
+        // if (cur_func_args != nullptr && cur_scope->parent->parent == nullptr) { // if not in table, search in function args
+        //     // cout << "not find in `Scope`" << cur_scope->sp_idx << " var_table, goto function arguments" << endl;
+        //     auto pair = cur_func_args->findInFuncArgs(var_name); 
+        //     if (pair.second.decl_type != TypeVoid) {
+        //         return SRC(new VirtReg(pair.first, pair.second, (cur_scope->parent == nullptr)));
+        //     }
+        // }
         // cout << "not find in `Scope`" << cur_scope->sp_idx << " var_table, goto parent table" << endl;
         cur_scope = cur_scope->parent;
     }
